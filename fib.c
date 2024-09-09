6c6343a4b0fb4bf6e353fd6507ca04e7632d7600
andrewdaitruong <andrewdaitruong@outlook.com>
2024-09-08 22:37:15 -0700

    Added the required information but could not test.

--- fib.c
+++ fib.c
@@ -1,15 +1,39 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
+//prototypes
 int fib_rec(int);
 
 int main() {
-   int n = 10;
-   int result = fib_rec(n);
-   printf("Fibonacci of %d is %d\n", n , result);
-   
+   //User input one.
+   int i;
+   printf("1st number: ", "\n");
+   scanf("%d", &i);
+
+   char filename[100];
+   scanf("%99s", filename);
+   int file_number = 0;
+   FILE *file = fopen(filename, "r");
+   if (fscanf(file, "%d", &file_number) != 1)
+   {
+      fclose(file);
+   }
+
+   //starts/ends the timer for the function
+   clock_t start_time = clock();
+   int n_number = i + file_number;
+   int result = fib_rec(n_number);
+   clock_t end_time = clock();
+   double function_timer = (double)(end_time - start_time) / CLOCKS_PER_SEC;
+
+   //simple print function for output
+   printf("Fibonacci of %d is %d\n", i, result);
+   printf("Time taken: %.6f seconds\n", function_timer);
    return 0;
 }
 
+//recursive function of fibanacci sequence
 int fib_rec (int i)
 {
    if (i < 0) return -1;
