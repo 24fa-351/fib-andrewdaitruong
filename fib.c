1d7ee448126c8ed638735b9dd380b4daa230d010
andrewdaitruong <andrewdaitruong@outlook.com>
2024-09-08 19:05:49 -0700

    Fib function working?

--- fib.c
+++ fib.c
@@ -1,6 +1,19 @@
 #include <stdio.h>
 
+int fib_rec(int);
+
 int main() {
-   printf("Hello!\n");
+   int n = 10;
+   int result = fib_rec(n);
+   printf("Fibonacci of %d is %d\n", n , result);
+   
    return 0;
+}
+
+int fib_rec (int i)
+{
+   if (i < 0) return -1;
+   if (i == 0) return 0;
+   if (i == 1) return 1;
+   return fib_rec(i - 1) + fib_rec (i - 2);
 }
